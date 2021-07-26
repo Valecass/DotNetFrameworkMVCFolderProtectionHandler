a04fc8ae74d287f114959267d331ed5e3a2bcfe2
Valerio Cassano <valerio.cassano@parravicinigiornali.it>
2021-07-26 15:39:00 +0200

    Initial commit

---
+++ src/DotNetFrameworkMVCFolderProtection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotNetFrameworkMVCFolderProtection.Startup))]
+namespace DotNetFrameworkMVCFolderProtection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
