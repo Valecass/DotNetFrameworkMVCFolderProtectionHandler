0c14a1a5e0ec05d4bc77c1801eba2e45afcb3f77
Valerio Cassano <valerio.cassano@parravicinigiornali.it>
2021-07-26 16:25:46 +0200

    initial commit

---
+++ src/MVCFolderProtection/MVCFolderProtection/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCFolderProtection.MvcApplication" Language="C#" %>
---
+++ src/MVCFolderProtection/MVCFolderProtection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCFolderProtection.Startup))]
+namespace MVCFolderProtection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
